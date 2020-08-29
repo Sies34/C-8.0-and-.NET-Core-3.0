77dc95d4071901fd2894f56ccc646ec28b43b64d
johan <jkuepers@hotmail.com>
2020-08-29 17:13:33 +0200

    Page 17

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C_8._0_and_.NET_Core_3._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, C#!");
+        }
+    }
+}
