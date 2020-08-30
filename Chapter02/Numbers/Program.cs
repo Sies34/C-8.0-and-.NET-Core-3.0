f1791dc2e3deb29a7fcde101209f51bd0c71e44d
Johan Kuepers <jkuepers@hotmail.com>
2020-08-30 19:53:02 +0200

    Sheet x

---
+++ Chapter02/Numbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // unsigned integer means positove whole number
+            // include 0
+            uint naturalNumber = 23;
+
+            // integer means negative or positive whole number
+            // include 0
+            int integerNumber = -23;
+
+            // float means single-precision floating point
+            // F suffix makes it a float literal
+            float realNumber = 2.3F;
+
+            // double means double-precision floating point
+            double anotherNumber = 2.3; //double literal
+        }
+    }
+}
