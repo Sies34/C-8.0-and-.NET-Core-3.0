2c24779f223c9f01b5b0141fdbdbdccd0d34ba58
Johan Kuepers <jkuep@NLMRN1L93YRM13.Worley.com>
2020-08-30 09:23:23 +0200

    Page 37

--- Chapter01/HelloCS/Program.cs
+++ Chapter01/HelloCS/Program.cs
@@ -5,9 +5,7 @@ namespace HelloCS
     class Program
     {
         static void Main(string[] args)
-        {
-            int z;
-            
+        {    
             Console.WriteLine("Hello, C#!");
         }
     }
