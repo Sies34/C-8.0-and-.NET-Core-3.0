58757c00d3f9afd892db2ce1e895616000b09fbe
johan <jkuepers@hotmail.com>
2020-08-31 20:59:42 +0200

    Page 57

---
+++ Chapter02/NullHandling/Program.cs
@@ -0,0 +1,36 @@
+﻿#nullable enable
+using System;
+
+namespace NullHandling
+{
+    class Address
+    {
+        public string? Building;
+        public string Street = string.Empty;
+        public string City = string.Empty;
+        public string Region = string.Empty;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // int thisCannotBeNull = 4;
+            // thisCannotBeNull = null; // compile error!
+
+            int? thisCoulsBeNull = null;
+            Console.WriteLine(thisCoulsBeNull);
+            Console.WriteLine(thisCoulsBeNull.GetValueOrDefault());
+
+            thisCoulsBeNull = 7;
+            Console.WriteLine(thisCoulsBeNull);
+            Console.WriteLine(thisCoulsBeNull.GetValueOrDefault());
+
+            var address = new Address();
+            address.Building = null;
+            address.Street = null;
+            address.City = "London";
+            address.Region = null;
+            
+        }
+    }
+}
---
+++ Chapter02/Variables/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int population = 66_000_000; // 66 million in UK
+            double weight =  1.88; // in kilograms
+            decimal price = 4.99M; // in pounds sterling
+            string fruit = "Apples"; //strings use double-quotes
+            char letter = 'Z'; //chars use singles-quotes
+            bool happy = true; //Booleans have value of true or false
+
+            // var population = 66_000_000; // 66 million in UK
+            // var weight =  1.88; // in kilograms
+            // var price = 4.99M; // in pounds sterling
+            // var fruit = "Apples"; //strings use double-quotes
+            // var letter = 'Z'; //chars use singles-quotes
+            // var happy = true; //Booleans have value of true or false
+
+            Console.WriteLine($"default(int) = {default(int)}");
+            Console.WriteLine($"default(bool) = {default(bool)}");
+            Console.WriteLine($"default(DateTime) = {default(DateTime)}");
+            Console.WriteLine($"default(string) = {default(string)}");
+
+        }
+    }
+}
