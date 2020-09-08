3ea54ecaf90ffb3b51f7c55fdc2cc178875137c4
Johan Kuepers <jkuep@NLMRN1L93YRM13.Worley.com>
2020-09-08 21:22:41 +0200

    Page 78

--- Chapter03/Operators/Program.cs
+++ Chapter03/Operators/Program.cs
@@ -14,6 +14,25 @@ namespace Operators
             int c = 3;
             int d = ++c; // increment c before addigning it
             WriteLine($"c is {c}, c is {d}");
+
+            int e = 11;
+            int f = 3;
+            WriteLine($"e is {e}, f is {f}");
+            WriteLine($"e + f = {e + f}");
+            WriteLine($"e - f = {e - f}");
+            WriteLine($"e * f = {e * f}");
+            WriteLine($"e / f = {e / f}");
+            WriteLine($"e % f = {e % f}");
+
+            double g = 11.0;
+            WriteLine($"g is {g:N1}, f is {f}");
+            WriteLine($"g / f = {g / f}");
+
+            int p = 6;
+            p += 3; // equivalent to p = p + 3
+            p -= 3; // equivalent to p = p - 3
+            p *= 3; // equivalent to p = p * 3
+            p /= 3; // equivalent to p = p / 3
         }
     }
 }
