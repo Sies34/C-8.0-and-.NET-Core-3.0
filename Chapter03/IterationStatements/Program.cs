75dace31e74732a4a4a533a75c024879c8887d86
Johan Kuepers <jkuep@NLMRN1L93YRM13.Worley.com>
2020-09-15 21:19:12 +0200

    Page 89

---
+++ Chapter03/IterationStatements/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using static System.Console;
+
+namespace IterationStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            
+            while (x < 10)
+            {
+                WriteLine(x);
+                x++;
+            }
+
+            string password = string.Empty;
+
+            int attempts = 0;
+            
+            do
+            {
+                WriteLine("Enter your password: ");
+                password = ReadLine();
+                attempts++;
+            }
+            while (password != "Pa$$w0rd" & attempts < 10);
+            
+            if (attempts >= 10)
+            {
+                WriteLine("error");
+            }
+            else
+            {
+                WriteLine("Correct");
+            }
+
+            for (int y = 1; y <= 10; y++)
+            {
+                WriteLine(y);
+            }
+
+            string[] names = {"Johan", "Esther", "Cas", "Job"};
+
+            foreach (string name in names)
+            {
+                WriteLine($"{name} has {name.Length} characters.");
+            }
+                                    
+        }
+    }
+}
