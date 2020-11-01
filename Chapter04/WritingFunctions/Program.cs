350c5c41efb15b03890f868da8bb4143adedd929
Johan Kuepers <jkuepers@hotmail.com>
2020-11-01 09:16:27 +0100

    Sheet 117

--- Chapter04/WritingFunctions/Program.cs
+++ Chapter04/WritingFunctions/Program.cs
@@ -87,7 +87,7 @@ namespace WritingFunctions
             if (decimal.TryParse(amountInText, out decimal amount))
             {
                 decimal taxToPay = CalculateTax(amount,region.ToUpper());
-                WriteLine($"You must pay {taxToPay} in sales tax.");
+                WriteLine($"You must pay {region.ToUpper()} {taxToPay} in sales tax.");
             }
             else
             {
@@ -95,10 +95,89 @@ namespace WritingFunctions
             }
         }
 
+        static string CardinalToOrdinal(int number)
+        {
+            switch(number)
+            {
+                case 11:
+                case 12:
+                case 13:
+                    return $"{number}th";
+                default:
+                    string numberAsText = number.ToString();
+                    char lastDigit = numberAsText[numberAsText.Length - 1];
+                    string suffix = string.Empty;
+                    switch (lastDigit)
+                    {
+                        case '1':
+                            suffix = "st";
+                            break;
+                        case '2':
+                            suffix = "nd";
+                            break;
+                        case '3':
+                            suffix = "rd";
+                            break;
+                        default:
+                            suffix = "th";
+                            break;
+                    }
+                    return $"{number}{suffix}";
+            }
+        }
+
+        static void RunCardinalToOrdinal()
+        {
+            for (int number = 1; number <= 40; number++)
+            {
+                Write($"{CardinalToOrdinal(number)} ");
+            }
+            WriteLine();
+        }
+
+        static int Factorial(int number)
+        {
+            if (number < 1)
+            {
+                return 0;
+            }
+            else if (number == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return number * Factorial(number - 1);
+            }
+        }
+
+        static void RunFactorial()
+        {
+            bool isNumber;
+            do
+            {
+                Write("Enter a number: ");
+
+                isNumber = int.TryParse(ReadLine(), out int number);
+
+                if (isNumber)
+                {
+                    WriteLine($"{number:N0}! = {Factorial(number):N0}");
+                }
+                else
+                {
+                    WriteLine("You did not enter a valid number!");
+                }
+            }
+            while (isNumber);
+        }
+
         static void Main(string[] args)
         {
             // RunTimesTable();
-            RunCalculateTax();
+            // RunCalculateTax();
+            // RunCardinalToOrdinal();
+            RunFactorial();
         }
     }
 }
